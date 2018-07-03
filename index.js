452540647c6cdc14c566bce97b4b9819ec0929bf
Elias Frostensson <frost.sunds@gmail.com>
2018-07-03 21:39:27 +0200

    Added a user counter.

--- index.js
+++ index.js
@@ -206,7 +206,21 @@ bot.on('message', async message => {
         .then(messages => message.channel.send(`**Successfully deleted \`${messages.size - 1}/${args[0]}\` messages**`)	
         .then(message => message.delete(10000)));	
     }
-
+    
+    if (cmd == `${prefix}users`) {
+        if (message.guild.id != '380414605744275456') return message.channel.send('Shhhh, this only works in Siaffs private server!');
+        let embed = new Discord.RichEmbed()
+            .setAuthor(message.guild.name, message.guild.iconURL())
+            .setColor('#0099ff')
+            .setThumbnail(message.guild.iconURL())
+            .addField('Members', `**${message.guild.memberCount}**`, true)
+            .addBlankField(true)
+            .addField('Humans', `**${message.guild.members.filter(member => !member.user.bot).size}**`, true)
+            .addField('Bots', `**${message.guild.members.filter(member => member.user.bot).size}**`, true)
+            .addField('Member Status', `**${message.guild.members.filter(o => o.presence.status === 'online').size}** Online\n**${message.guild.members.filter(i => i.presence.status === 'idle').size}** Idle/Away\n**${message.guild.members.filter(dnd => dnd.presence.status === 'dnd').size}** Do Not Disturb\n**${message.guild.members.filter(off => off.presence.status === 'offline').size}** Offline/Invisible\n**${message.guild.members.filter(s => s.presence.status === 'streaming').size}** Streaming`)
+            .setFooter(`Owner: ${message.guild.owner.user.tag}`)
+        message.channel.send(embed);
+    }
 
 });
 
